f3da8cb4a691fc0908a30ba87e47ac85f82ce7b6
Tony <tony.tao@bindo.com>
2017-11-11 23:38:17 +0800

    add progress time
    add progress time

--- js/man.js
+++ js/man.js
@@ -5,8 +5,11 @@ window.onload = function () {
     var play = document.getElementById('play')
     var pause = document.getElementById('pause')
     var reloadProgressDOM = document.getElementById('progressBar')
+    var progressTimeShowDOM = document.getElementById('vplayerTimeLine')
     var _playerControlHidden,_playTime
 
+    //3000ms hidden player
+    playerControlHidden()
     //play
     play.addEventListener('click',vPlayerPlay)
     //pause
@@ -15,7 +18,6 @@ window.onload = function () {
     playerContent.addEventListener('mouseover',playerControlShow)
     //hidden player control
     playerContent.addEventListener('mouseleave',playerControlHidden)
-    console.log(playerControl.style.opacity)
     function vPlayerPlay() {
         progressBar()
         playerContent.play()
@@ -52,17 +54,45 @@ window.onload = function () {
             //reload progress time (加载的时间/总时间)*100 = 加载的百分比
             var reloadProgressTime = (playerContent.buffered.end(0) / playerContent.duration) * 100
             reloadProgressDOM.children[0].style.width = reloadProgressTime + '%'
-            // console.log(reloadProgressDOM,reloadProgressDOM.children[0])
+
             //progress time = (playerContent.currentTime/playerContent.duration)*100
             var progressTime = (playerContent.currentTime / playerContent.duration) * 100
             reloadProgressDOM.children[0].children[0].style.width = progressTime + '%'
+
+            //progress time show
+            progressTimeShowDOM.innerText = secToTime(progressTime)+' | '+secToTime(playerContent.duration)
             // console.log(playerContent.duration,playerContent.currentTime,playerContent.buffered.end(0))
             if(playerContent.currentTime === playerContent.duration){
                 clearInterval(_playTime)
                 vPlayerPause()
+                reloadProgressDOM.children[0].children[0].style.width = 0
             }
-        })
+        },0)
 
 
+    }
+
+    /**
+     * 时间秒数格式化
+     * @param s 秒数
+     * @returns {*} 格式化后的时分秒
+     */
+    var secToTime = function (s) {
+        var t;
+        if(s > -1){
+            var hour = Math.floor(s/3600);
+            var min = Math.floor(s/60) % 60;
+            var sec = s % 60;
+            if(hour < 10) {
+                t = '0'+ hour + ":";
+            } else {
+                t = hour + ":";
+            }
+            if(min < 10){t += "0";}
+            t += min + ":";
+            if(sec < 10){t += "0";}
+            t += sec.toFixed(0);
+        }
+        return t;
     }
 }
